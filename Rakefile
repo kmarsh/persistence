1769bcc433263551a7dd5c4a1eafd33b14b5e73b
Kevin Marsh <kmarsh@formandfx.com>
2010-03-28 22:31:43 -0400

    Can I run a simple rakefile on heroku via sinatra?

---
+++ Rakefile
@@ -0,0 +1,4 @@
+task :default do
+  $stderr.puts "rake"
+  $stdout.puts "[this would be a test]\n........\n"
+end
--- persistence.rb
+++ persistence.rb
@@ -1,10 +1,21 @@
 require 'rubygems'
 require 'sinatra'
+require "open3"
 
+include Open3
+  
 get '/' do
   "Hello from Sinatra on Heroku!"
 end
 
 post '/build' do
-  params.inspect
+  PersistanceBuilder.build
+end
+
+class PersistanceBuilder
+  def self.build
+    stdin, stdout, stderr = popen3('rake')
+    
+    return stdout
+  end
 end
